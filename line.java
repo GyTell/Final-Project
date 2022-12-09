1b4047e78cfb338d9e3f993035389f91f4a4d4c6
GyTell <112898609+GyTell@users.noreply.github.com>
2022-12-09 16:49:57 -0500

    Add files via upload

---
+++ line.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class line {
+
+	@Test
+	public void test() {
+		new WordCounterFour();
+		String line = null;
+		assertEquals (null, line);	}
+
+}
+//https://www.softwaretestinghelp.com/junit-tests-examples/
